8560db379f934028da6ebf23ee8db56ddcceff0d
yyb4j <17796553126@163.com>
2023-12-12 23:15:23 +0800

    crudRepository 改为JpaRepository

--- proj_common/src/main/java/com/example/proj_common/security/repository/MenuRepository.java
+++ proj_common/src/main/java/com/example/proj_common/security/repository/MenuRepository.java
@@ -1,7 +1,7 @@
 package com.example.proj_common.security.repository;
 
 import com.example.proj_common.security.entity.Menu;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MenuRepository extends CrudRepository<Menu, Long> {
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
--- proj_common/src/main/java/com/example/proj_common/security/repository/UserRepository.java
+++ proj_common/src/main/java/com/example/proj_common/security/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.proj_common.security.repository;
 
 import com.example.proj_common.security.entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
