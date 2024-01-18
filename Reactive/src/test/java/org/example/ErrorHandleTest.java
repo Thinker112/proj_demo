2cc8f6403e3dc2238155e7387cf1bd3b2c856d80
yyb4j <17796553126@163.com>
2024-01-18 22:36:47 +0800

    Flux error handle

--- Reactive/src/test/java/org/example/ErrorHandleTest.java
+++ Reactive/src/test/java/org/example/ErrorHandleTest.java
@@ -14,11 +14,13 @@ public class ErrorHandleTest {
     @Test
     public void errorHandleTest(){
         Flux<String> flux = Flux.just(1, 2, 0)
-//                .log()
                 .map(i -> "100 / " + i + " = " + (100 / i)) //this triggers an error with 0
-                .log()
+//                .log()
+//                .onErrorResume(err -> Mono.just("resume :)"));
+                .onErrorMap(err -> new RuntimeException("map -> pam"));//重新抛出自定义异常
+//                .log();
+//                .onErrorReturn(NullPointerException.class, "空指针异常");
 //                .onErrorReturn("Divided by zero :(");// error handling example
-                .onErrorComplete();//将错误信号替换为完成信号
         flux.log().subscribe();
 
         try {
@@ -27,4 +29,45 @@ public class ErrorHandleTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    public void doOnError() {
+        Flux.just(1, 2, 0, 3, 4, 5)
+                .map(i -> "100 / " + i + " = " + (100 / i))
+                .doOnError(err -> System.out.println("onError已被记录: " + err))
+                .doFinally(signal -> System.out.println("signal = " + signal))
+                .subscribe(v -> System.out.println("v = " + v),
+                        err -> System.out.println("onError被处理: " + err),
+                        () -> System.out.println("流完成信号"));
+    }
+
+    @Test
+    public void onErrorContinue() {
+        Flux.just(1, 2, 0, 3, 4, 5)
+                .map(i -> "100 / " + i + " = " + (100 / i))
+                .onErrorContinue((err, o) -> System.out.println("onErrorContinue被记录: " + err + " " + o + " -> 继续执行"))
+                .subscribe(v -> System.out.println("v = " + v),
+                        err -> System.out.println("onError被处理: " + err),
+                        () -> System.out.println("流完成信号"));
+    }
+
+    @Test
+    public void onErrorComplete() {
+        Flux.just(1, 2, 0, 3, 4, 5)
+                .map(i -> "100 / " + i + " = " + (100 / i))
+                .onErrorComplete()//将错误信号替换为完成信号
+                .subscribe(v -> System.out.println("v = " + v),
+                        err -> System.out.println("onError被处理: " + err),
+                        () -> System.out.println("流完成信号"));
+    }
+
+    @Test
+    public void onErrorStop() {
+        Flux.just(1, 2, 0, 3, 4, 5)
+                .map(i -> "100 / " + i + " = " + (100 / i))
+                .onErrorStop()
+                .subscribe(v -> System.out.println("v = " + v),
+                        err -> System.out.println("onError被处理: " + err),
+                        () -> System.out.println("流完成信号"));
+            }
 }
