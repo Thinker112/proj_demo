e118bfe2ab1fea88571fc396953ab0f44f5e8704
yyb4j <17796553126@163.com>
2024-01-30 10:23:11 +0800

    test

--- design_patterns/Else/src/test/java/TestElse.java
+++ design_patterns/Else/src/test/java/TestElse.java
@@ -1,8 +1,11 @@
 import org.junit.Test;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class TestElse {
-
-
     @Test
     public void test01(){
         double num1 = 2;
@@ -11,5 +14,26 @@ public class TestElse {
         double l = num1 / num2;
 
         System.out.println("l = " + l);
+    }
+
+    @Test
+    public void timestampToLocalDate(){
+        long timestamp = 1706457600000L;
+        Instant instant = Instant.ofEpochMilli(timestamp);
+        LocalDate localDate = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate();
+        System.out.println("localDateTime = " + localDate);
+    }
+
+    @Test
+    public void getOSInfo(){
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            System.out.println("Windows");
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+            System.out.println("Unix/Linux/Mac");
+        } else {
+            System.out.println("Other");
+        }
     }
 }
