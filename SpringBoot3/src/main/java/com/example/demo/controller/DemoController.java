8c6cab1e9038cb2231783aa5d900c19ad3d58947
yyb4j <17796553126@163.com>
2024-06-24 19:28:36 +0800

    temporary commit

--- SpringBoot3/src/main/java/com/example/demo/controller/DemoController.java
+++ SpringBoot3/src/main/java/com/example/demo/controller/DemoController.java
@@ -4,10 +4,18 @@ import com.example.demo.annotation.Log;
 import com.example.demo.bean.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.ThreadContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.UUID;
 
 @RestController
@@ -28,12 +36,33 @@ public class DemoController {
         person.setId(100L);
         person.setEmail("fas@gmial.com");
         person.setUserName("tom");
-        log.info(person.toString() + arg);
+        log.info("{}{}", person, arg);
         return person;
     }
 
     @GetMapping("/response/entity")
     public ResponseEntity<?> responseEntity() {
         return ResponseEntity.ok("hello world");
+    }
+
+
+    private static final String IMAGE_PATH = "D://img01.jpg"; // 修改为实际的图片路径
+
+    @GetMapping("/image")
+    public ResponseEntity<byte[]> getImage() throws IOException {
+        File file = new File(IMAGE_PATH);
+        Resource resource = new FileSystemResource(file);
+
+        if (!resource.exists()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        byte[] imageBytes = Files.readAllBytes(file.toPath());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(imageBytes.length);
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
     }
 }
